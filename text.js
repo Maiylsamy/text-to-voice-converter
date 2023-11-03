f1af60a9ec206e2c39e60f38f19e2179ef071c3d
Maiylsamy <maiylsamy310@gmail.com>
2023-11-03 13:15:31 +0530

    text to voice converter done

---
+++ text.js
@@ -0,0 +1,17 @@
+let speech = new SpeechSynthesisUtterance();
+let press = document.querySelector("button");
+let voiceSelect = document.querySelector("select");
+
+let voices=[] ;
+window.speechSynthesis.onvoiceschanged=
+() =>{
+    voices=window.speechSynthesis.getVoices();
+    speech.voice=voices[0];
+
+    voices.forEach((voice,i) =>(voiceSelect.options[i]=new Option(voice.name,i)));
+}
+voiceSelect.addEventListener("change",()=>speech.voice=voices[voiceSelect.value])
+
+press.addEventListener("click",()=>{
+    speech.text=document.querySelector("textarea").value;
+    window.speechSynthesis.speak(speech)});                                                         
